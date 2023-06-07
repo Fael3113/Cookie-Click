6f1a2a754c850bf27484d65ca71325720b3c999e
Fael3113 <santiagorafaelsantiago@hotmail.com>
2023-06-07 17:34:41 -0300

    main

---
+++ src/cookie/clicker/Painel.java
@@ -0,0 +1,29 @@
+package cookie.clicker;
+
+import java.awt.Graphics;
+import javax.swing.ImageIcon;
+
+public class Painel extends javax.swing.JPanel{
+    private ImageIcon img;
+    
+    public Painel(){
+       img = new ImageIcon();
+    }
+    
+    @Override
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        
+        g.drawImage(img.getImage(), 0, 0, this.getWidth(), this.getHeight(), this);
+        
+    }
+    
+    public void setImg(ImageIcon img){
+        this.img = img;
+    }
+    
+    public ImageIcon getImg(){
+        return this.img;
+    }
+
+}
