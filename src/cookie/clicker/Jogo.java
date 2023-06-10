62c24ec2cbf6208cd6e6195f4cd9bff3baa6c54a
Fael3113 <santiagorafaelsantiago@hotmail.com>
2023-06-10 10:32:03 -0300

    main

--- src/cookie/clicker/Jogo.java
+++ src/cookie/clicker/Jogo.java
@@ -4,16 +4,15 @@
  */
 package cookie.clicker;
 
-import java.awt.Image;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Random;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 /**
@@ -21,13 +20,13 @@ import javax.swing.JOptionPane;
  * @author User
  */
 public class Jogo extends javax.swing.JFrame {
-    /**
-     * Creates new form Jogo
-     */
+    
+    public int cookieCounter;
+    public int ponto;
+    
     public Jogo() {
         initComponents();
     }
-    public int counter;
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -38,29 +37,32 @@ public class Jogo extends javax.swing.JFrame {
     private void initComponents() {
 
         painel1 = new cookie.clicker.Painel();
-        lblPontos = new javax.swing.JLabel();
-        btnClick = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        btnCookie = new javax.swing.JButton();
         txtPontos = new javax.swing.JTextField();
         btnResetar = new javax.swing.JButton();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jtxtMensagem = new javax.swing.JTextPane();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         mbtnSalvar = new javax.swing.JMenuItem();
         mbtnCarregar = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setPreferredSize(new java.awt.Dimension(1920, 1080));
 
         painel1.setImg(new ImageIcon("src/imagens/black_hole.jpg")
         );
 
-        lblPontos.setBackground(new java.awt.Color(255, 51, 51));
-        lblPontos.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel1.setBackground(new java.awt.Color(255, 255, 255));
+        jLabel1.setFont(new java.awt.Font("Rockwell", 1, 18)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel1.setText("Cookies");
 
-        btnClick.setBackground(new java.awt.Color(102, 51, 0));
-        btnClick.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/Cookie_JE2_BE2.jpg"))); // NOI18N
-        btnClick.addActionListener(new java.awt.event.ActionListener() {
+        btnCookie.setBackground(new java.awt.Color(102, 51, 0));
+        btnCookie.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/Cookie_JE2_BE2.jpg"))); // NOI18N
+        btnCookie.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnClickActionPerformed(evt);
+                btnCookieActionPerformed(evt);
             }
         });
 
@@ -76,31 +78,39 @@ public class Jogo extends javax.swing.JFrame {
             }
         });
 
+        jScrollPane2.setViewportView(jtxtMensagem);
+
         javax.swing.GroupLayout painel1Layout = new javax.swing.GroupLayout(painel1);
         painel1.setLayout(painel1Layout);
         painel1Layout.setHorizontalGroup(
             painel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(painel1Layout.createSequentialGroup()
                 .addGap(40, 40, 40)
-                .addGroup(painel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(lblPontos, javax.swing.GroupLayout.PREFERRED_SIZE, 26, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(txtPontos)
-                    .addComponent(btnClick, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(btnResetar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addContainerGap(1514, Short.MAX_VALUE))
+                .addGroup(painel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel1)
+                    .addGroup(painel1Layout.createSequentialGroup()
+                        .addGroup(painel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(txtPontos)
+                            .addComponent(btnCookie, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(btnResetar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addGap(18, 18, 18)
+                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 234, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(34, Short.MAX_VALUE))
         );
         painel1Layout.setVerticalGroup(
             painel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, painel1Layout.createSequentialGroup()
-                .addGap(60, 60, 60)
-                .addGroup(painel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(lblPontos, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(txtPontos, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
-                .addComponent(btnClick)
-                .addGap(65, 65, 65)
+                .addGap(44, 44, 44)
+                .addComponent(jLabel1)
+                .addGap(12, 12, 12)
+                .addComponent(txtPontos, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(37, 37, 37)
+                .addGroup(painel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(btnCookie)
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 93, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(52, 52, 52)
                 .addComponent(btnResetar)
-                .addContainerGap(496, Short.MAX_VALUE))
+                .addContainerGap(58, Short.MAX_VALUE))
         );
 
         jMenu1.setText("File");
@@ -129,7 +139,9 @@ public class Jogo extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(painel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(painel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -138,11 +150,25 @@ public class Jogo extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    private void btnClickActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnClickActionPerformed
-        counter = Integer.parseInt(txtPontos.getText());
-        txtPontos.setText(Integer.toString(++counter));
-    }//GEN-LAST:event_btnClickActionPerformed
+    
+    private void btnCookieActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCookieActionPerformed
+        cookieCounter = Integer.parseInt(txtPontos.getText());
+        txtPontos.setText(Integer.toString(++cookieCounter));
+        Random random = new Random();
+        ponto = random.nextInt(10)+1;
+        if (cookieCounter > 50 && cookieCounter < 99) {
+            txtPontos.setText(Integer.toString(ponto+cookieCounter));
+        } else if (cookieCounter > 100 && cookieCounter < 999) {
+            txtPontos.setText(Integer.toString(15+cookieCounter));
+        } else if (cookieCounter > 1000 && cookieCounter < 10000) {
+            txtPontos.setText(Integer.toString((ponto*3)+cookieCounter));
+            jtxtMensagem.setText("Parabens, voce chegou aos 1000 cookies, ao infinito e alem!");
+        } else if (cookieCounter > 10000) {
+            txtPontos.setText(Integer.toString((ponto*5)+cookieCounter));
+            jtxtMensagem.setText("Pare pessoa, você vai quebrar o seu mouse ainda!!");
+        }
+        
+    }//GEN-LAST:event_btnCookieActionPerformed
 
     private void mbtnSalvarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mbtnSalvarActionPerformed
         salvamento();
@@ -196,11 +222,13 @@ public class Jogo extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnClick;
+    private javax.swing.JButton btnCookie;
     private javax.swing.JButton btnResetar;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenuBar jMenuBar1;
-    private javax.swing.JLabel lblPontos;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JTextPane jtxtMensagem;
     private javax.swing.JMenuItem mbtnCarregar;
     private javax.swing.JMenuItem mbtnSalvar;
     private cookie.clicker.Painel painel1;
@@ -222,19 +250,5 @@ public class Jogo extends javax.swing.JFrame {
         String arquvio = new String(Files.readAllBytes(Paths.get("src/arquivotxt/pontuacao.txt")));
         txtPontos.setText(arquvio);
     }
-    
-    public void carregar1(){
-        try {
-            String linha;
-            File arquivo = new File("src/arquivotxt/pontucao.txt");
-            Scanner leitor = new Scanner(arquivo);
-            while (leitor.hasNextLine()) {              
-                linha = leitor.nextLine();
-                txtPontos.setText(linha);
-            }
-            leitor.close(); 
-        } catch (IOException e) {
-            JOptionPane.showMessageDialog(rootPane, "Não foi possivel carregar o arquivo");
-        }
-    }
+
 }
